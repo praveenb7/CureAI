7da36158199e5ee79b681ad07db7683d31c6bc7a
Benson Thomas <52985692+volvox747@users.noreply.github.com>
2021-12-22 09:35:17 +0530

    Update upload.js

--- CureAI/static/js/upload.js
+++ CureAI/static/js/upload.js
@@ -28,3 +28,37 @@ function dropdown(e) {
     console.log("ERROR", e);
   }
 }
+
+
+// Generate Result
+
+function generate(file)
+{
+  try{
+  let endpoint = document.getElementById("dropdownMenuButton1");
+  const API_ENDPOINT = `http://192.168.225.116:5000/tuberculosis`;
+  const request = new XMLHttpRequest();
+  const formData = new FormData();
+
+  request.open("POST", API_ENDPOINT, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.responseText);
+    }
+  };
+  formData.append("file", file);
+  let result=request.send(formData);
+
+  }catch(e)
+  {
+    console.log("error message: ",e);
+  }
+}
+
+
+let fileinput = document.getElementById("inputGroupFile04");
+fileinput.addEventListener("change", (event) => {
+  const files = event.target.files;
+  console.log(files);
+  generate(files[0]);
+});
