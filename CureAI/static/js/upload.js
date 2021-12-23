7114c7f2986928f0ce1f13e191dd94759cac4aec
Praveen B <praveen6312@gmail.com>
2021-12-23 22:12:36 +0530

    Modified upload.html, upload.css and upload.js

--- CureAI/static/js/upload.js
+++ CureAI/static/js/upload.js
@@ -19,12 +19,63 @@ function showPreview(event) {
 
 // Dropdown button 
 
+let dropdownMenuButton1 = document.getElementById("dropdownMenuButton1");
 function dropdown(e) {
   try {
-    dropdownMenuButton1 = document.getElementById("dropdownMenuButton1");
     if (e.target.innerText != "") 
       dropdownMenuButton1.innerText = e.target.innerText;
+
   } catch (e) {
     console.log("ERROR", e);
   }
 }
+
+
+
+// Generate button
+
+
+
+async function generate(file)
+{
+  try{
+  let endpoint = dropdownMenuButton1.innerText.toLowerCase().replace(" ","-");
+  let condition= document.getElementById("condition");
+  let description = document.getElementById("descript");
+
+
+
+  //$ Main Code 
+
+  const API_ENDPOINT = `http://192.168.125.128:5000/${endpoint}`;
+  const request = new XMLHttpRequest();
+  const formData = new FormData();
+
+  request.open("POST", API_ENDPOINT, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      let result = JSON.parse(request.responseText);
+      console.log(result.msg);
+      condition.innerText=result.msg;
+      description.innerText=result.desc;
+
+
+    }
+  };
+  formData.append("file", file);
+  request.send(formData);  
+
+
+  }catch(e)
+  {
+    console.log("error message: ",e);
+  }
+}
+
+
+let fileinput = document.getElementById("inputGroupFile04");
+fileinput.addEventListener("change", (event) => {
+  const files = event.target.files;
+  console.log(files);
+  generate(files[0]);
+});
