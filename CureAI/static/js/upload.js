028af4bf95b479045187a34e495865c19f10070e
Praveen B <praveen6312@gmail.com>
2021-12-31 18:53:17 +0530

    Improved UI

--- CureAI/static/js/upload.js
+++ CureAI/static/js/upload.js
@@ -1,5 +1,5 @@
-// Image Preview 
 
+// Image Preview 
 function showPreview(event) {
   try {
 
@@ -11,71 +11,146 @@ function showPreview(event) {
       // Upating the '--text' variable in CSS to flex 
       document.documentElement.style.setProperty('--text', 'flex');
     }
-  } catch (e) {
+  } 
+  catch (e) {
     console.log("ERROR", e);
   }
 }
 
+
+var generatebtn = document.getElementById("generate");
 
 // Dropdown button 
+var dropdownname = '';
+var dropdownMenuButton1 = document.getElementById("dropdownMenuButton1");
 
-let dropdownMenuButton1 = document.getElementById("dropdownMenuButton1");
+
+
+// Function to check if dropdown item has valid name
+function dropdowncheck() {
+  return dropdownname.localeCompare('')==0 || dropdownname.localeCompare('select-disease')==0;
+}
+
+
+
+// Function to decide whether modal should be shown or not(by checking if all inputs are provided by the user)
+function finalcheck() {
+  if(imgfile.length > 0 && imgfile[0] != undefined && (!dropdowncheck())) {
+    generatebtn.setAttribute("data-bs-toggle", "modal");
+    generatebtn.setAttribute("data-bs-target", "#exampleModal");
+  }
+  else {
+    if(generatebtn.hasAttribute("data-bs-toggle")) {
+      generatebtn.removeAttribute("data-bs-toggle");
+    }
+    else if(generatebtn.hasAttribute("data-bs-target")) {
+      generatebtn.removeAttribute("data-bs-target");
+    }
+  }
+}
+
+
+
+// Dropdown Onclick listener
 function dropdown(e) {
   try {
-    if (e.target.innerText != "") 
+    const s = e.target.innerText;
+    if(!(s.localeCompare("")==0 && s.localeCompare("select-disease")==0)) {
       dropdownMenuButton1.innerText = e.target.innerText;
+      dropdownname = dropdownMenuButton1.innerText;
 
-  } catch (e) {
+      finalcheck();
+    }
+  } 
+  catch (e) {
     console.log("ERROR", e);
   }
+
+  // console.log(generatebtn.getAttributeNames());
 }
 
 
 
-// Generate button
-
-
-
+// Function to fetch the results from the API
 async function generate(file)
 {
-  try{
-  let endpoint = dropdownMenuButton1.innerText.toLowerCase().replace(" ","-");
-  let condition= document.getElementById("condition");
-  let description = document.getElementById("descript");
+  try {
+    let endpoint = dropdownMenuButton1.innerText.toLowerCase().replace(" ","-");
+  
+    let condition= document.getElementById("condition");
+    let description = document.getElementById("descript");
+  
+    condition.innerText="";
+    description.innerText="";
+  
+    let loading = document.getElementsByClassName("loading")[0];
+    loading.style.display = "flex";
+  
+    //$ Main Code 
+  
+    const API_ENDPOINT = `http://localhost:5000/${endpoint}`;
+    const request = new XMLHttpRequest();
+    const formData = new FormData();
+  
+    request.open("POST", API_ENDPOINT, true);
 
-
-
-  //$ Main Code 
-
-  const API_ENDPOINT = `http://192.168.125.128:5000/${endpoint}`;
-  const request = new XMLHttpRequest();
-  const formData = new FormData();
-
-  request.open("POST", API_ENDPOINT, true);
-  request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-      let result = JSON.parse(request.responseText);
-      console.log(result.msg);
-      condition.innerText=result.msg;
-      description.innerText=result.desc;
-
-
-    }
-  };
-  formData.append("file", file);
-  request.send(formData);  
-
-
-  }catch(e)
-  {
+    request.onreadystatechange = () => {
+      if (request.readyState === 4 && request.status === 200) {
+        let result = JSON.parse(request.responseText);
+        loading.style.display = "none";
+        // console.log(result.msg);
+        condition.innerText=result.msg;
+        description.innerText=result.desc;
+      }
+      else{
+        loading.style.display = "none";
+        description.innerText="Something went wrong... Try again later!";
+      }
+    };
+    formData.append("file", file);
+    request.send(formData);  
+  
+  }
+  catch(e) {
     console.log("error message: ",e);
   }
+  
 }
 
 
+
+var imgfile = [];
 let fileinput = document.getElementById("inputGroupFile04");
+
+
 fileinput.addEventListener("change", (event) => {
   const files = event.target.files;
-  console.log(files);
-  generate(files[0]);
+  // console.log(files);
+  imgfile[0] = files[0];
+
+  finalcheck();
+
+  if(imgfile.length > 0 && imgfile[0] != undefined) {
+    document.getElementById("img-preview").style.display = "var(--text)";
+  }
+  else {
+    document.getElementById("img-preview").style.display = "none";
+  }
+
+  // console.log(generatebtn.getAttributeNames());
+});
+
+
+
+generatebtn.addEventListener("click", () => {
+  console.log("imgfile:", imgfile);
+  if(imgfile.length > 0 && imgfile[0] != undefined && (!dropdowncheck())) {
+    generate(imgfile[0]);
+  }
+  else if(dropdowncheck()) {
+    alert("Select a disease!");
+  }
+  else {
+    alert("No Image Selected!");
+  }
 });
